d1381fc391583a42c5e1e82ae590967d2da01414
Insaider <levadb9@gmail.com>
2023-08-24 17:02:37 +1000

    first commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,83 +1,90 @@
-# flake8: noqa
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOST',
+                          default='127.0.0.1, localhost').split(', ')
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework.authtoken',
-    'rest_framework',
-    'djoser',
-    'cats.apps.CatsConfig',
+        'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'rest_framework.authtoken',
+        'rest_framework',
+        'djoser',
+        'cats.apps.CatsConfig',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        'django.middleware.security.SecurityMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'kittygram_backend.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        {
+                'BACKEND': 'django.template.backends.django.DjangoTemplates',
+                'DIRS': [],
+                'APP_DIRS': True,
+                'OPTIONS': {
+                        'context_processors': [
+                                'django.template.context_processors.debug',
+                                'django.template.context_processors.request',
+                                'django.contrib.auth.context_processors.auth',
+                                'django.contrib.messages.context_process'
+                                'ors.messages',
+                        ],
+                },
         },
-    },
 ]
 
 WSGI_APPLICATION = 'kittygram_backend.wsgi.application'
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'default': {
+                'ENGINE': os.getenv('ENGINE', 'django.db.backends.sqlite3'),
+                'NAME': os.getenv('POSTGRES_DB', 'kittygram'),
+                'USER': os.getenv('POSTGRES_USER', 'kittygram_user'),
+                'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+                'HOST': os.getenv('DB_HOST', ''),
+                'PORT': os.getenv('DB_PORT', 5432)
+        }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+        {
+                'NAME': 'django.contrib.auth.password_validation'
+                        '.UserAttributeSimilarityValidator',
+        },
+        {
+                'NAME': 'django.contrib.auth.password_validation'
+                        '.MinimumLengthValidator',
+        },
+        {
+                'NAME': 'django.contrib.auth.password_validation'
+                        '.CommonPasswordValidator',
+        },
+        {
+                'NAME': 'django.contrib.auth.password_validation'
+                        '.NumericPasswordValidator',
+        },
 ]
 
 LANGUAGE_CODE = 'en-us'
@@ -90,8 +97,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -99,15 +106,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
-    ],
+        'DEFAULT_PERMISSION_CLASSES': [
+                'rest_framework.permissions.IsAuthenticated',
+        ],
 
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+        'DEFAULT_AUTHENTICATION_CLASSES': [
+                'rest_framework.authentication.TokenAuthentication',
+        ],
 
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+        'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 10,
 
 }
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 import pytest
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 BACKEND_DIR_NAME = 'backend'
 FRONTEND_DIR_NAME = 'frontend'
@@ -19,8 +18,9 @@ for dir_name in (BACKEND_DIR_NAME, FRONTEND_DIR_NAME, NGINX_DIR_NAME):
     path_to_dir = BASE_DIR / dir_name
     if not path_to_dir.is_dir():
         raise AssertionError(
-            f'В директории `{BASE_DIR}` не найдена папка проекта '
-            f'`{dir_name}/`. Убедитесь, что у вас верная структура проекта.'
+                f'В директории `{BASE_DIR}` не найдена папка проекта '
+                f'`{dir_name}/`. Убедитесь, что у вас верная структура '
+                f'проекта.'
         )
 
 
@@ -63,8 +63,8 @@ def workflow_file_name() -> str:
 def deploy_file_info() -> tuple[Path, str]:
     deploy_info_file = BASE_DIR / DEPLOY_INFO_FILE_NAME
     assert deploy_info_file.is_file(), (
-        f'Убедитесь, что в корневой директории проекта создан файл '
-        f'`{DEPLOY_INFO_FILE_NAME}`'
+            f'Убедитесь, что в корневой директории проекта создан файл '
+            f'`{DEPLOY_INFO_FILE_NAME}`'
     )
     return (deploy_info_file, DEPLOY_INFO_FILE_NAME)
 
@@ -72,7 +72,7 @@ def deploy_file_info() -> tuple[Path, str]:
 @pytest.fixture(scope='session')
 def deploy_info_file_content(
         deploy_file_info: tuple[Path, str]
-        ) -> dict[str, str]:
+) -> dict[str, str]:
     path, relative_path = deploy_file_info
     with open(path, 'r', encoding='utf-8', errors='ignore') as f:
         file_content = {}
@@ -81,10 +81,10 @@ def deploy_info_file_content(
             if not line.strip():
                 continue
             assert line_pattern.match(line), (
-                f'Убедитесь, что строка номер {line_num} файла '
-                f'`{relative_path}` соответствует шаблону: '
-                '`<ключ>: <значение>`. В названии ключа '
-                'допустимы буквы и нижнее подчеркивание.'
+                    f'Убедитесь, что строка номер {line_num} файла '
+                    f'`{relative_path}` соответствует шаблону: '
+                    '`<ключ>: <значение>`. В названии ключа '
+                    'допустимы буквы и нижнее подчеркивание.'
             )
             line = line.strip().strip(';')
             key, value = line.split(':', maxsplit=1)
@@ -95,10 +95,10 @@ def deploy_info_file_content(
 @pytest.fixture(scope='session')
 def expected_deploy_info_file_content() -> dict[str, str]:
     return {
-        'repo_owner': 'ваше имя пользователя на GitHub',
-        TASKI_DOMAIN_KEY: 'ссылка для доступа к проекту `Taski`',
-        KITTYGRAM_DOMAIN_KEY: 'ссылка для доступа к проекту Kittygram',
-        'dockerhub_username': 'ваше имя пользователя на Docker Hub',
+            'repo_owner': 'ваше имя пользователя на GitHub',
+            TASKI_DOMAIN_KEY: 'ссылка для доступа к проекту `Taski`',
+            KITTYGRAM_DOMAIN_KEY: 'ссылка для доступа к проекту Kittygram',
+            'dockerhub_username': 'ваше имя пользователя на Docker Hub',
     }
 
 
